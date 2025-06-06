6f8ade8fceb4c14484fb20cca2052cd50ab71e6f
2024-DEV3-007 <reshmaps@gmail.com>
2025-06-06 20:36:37 +0530

    feat : Decrement sellIn value of Aged Brie items

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GildedRoseTest {
+
+    @Test
+    @DisplayName ("Aged Brie : SellIn value reduces by one for each day")
+    void updateSellInDaysForAgedBrie () {
+
+        Item[] items = new Item[]{new Item ("Aged Brie", 1, 1)};
+        GildedRose app = new GildedRose (items);
+        app.updateQuality ();
+        assertEquals (0, items[0].sellIn);
+    }
+}
