4be89b63f6aba02d7a0d41929a0e2a9d8cdb756f
2024-DEV3-007 <reshmaps@gmail.com>
2025-06-07 15:51:25 +0530

    feat : Decrement sellIn value of default items

---
+++ src/test/java/com/gildedrose/updater/DefaultItemTest.java
@@ -0,0 +1,28 @@
+package com.gildedrose.updater;
+
+import com.gildedrose.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DefaultItemTest {
+
+    private DefaultItem defaultItem;
+
+    @BeforeEach
+    void setUp () {
+        defaultItem = new DefaultItem ();
+    }
+
+    @Test
+    @DisplayName("Default item : SellIn decreases by 1 each day")
+    void updateSellInDaysForDefaultItem () {
+
+        Item item = new Item ("Elixir of the Mongoose", 10, 20);
+
+        defaultItem.update (item);
+
+        assertEquals (9, item.sellIn);
+    }
+}
