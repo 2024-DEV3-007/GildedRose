14ff1896e5759f7e306e0fa729fc120690b20378
2024-DEV3-007 <reshmaps@gmail.com>
2025-06-08 15:23:42 +0530

    refactor : Modified the conjured item methods for reusability

--- src/main/java/com/gildedrose/updater/Conjured.java
+++ src/main/java/com/gildedrose/updater/Conjured.java
@@ -2,12 +2,19 @@ package com.gildedrose.updater;
 
 import com.gildedrose.Item;
 import static com.gildedrose.validation.ItemValidator.*;
+import static com.gildedrose.constant.Constants.*;
+
 public class Conjured implements ItemUpdater{
 
     @Override
     public void update (Item item) {
-        int decrementValue = (item.sellIn > 0) ? 2 :4;
-        decrementQuality(item,decrementValue);
-        updateSellInDays(item);
+        decrementQuality (item, DOUBLE_QUALITY_DECREMENT);
+        updateSellInDays (item);
+        updateQualityForExpiredItem (item);
+    }
+
+    private static void updateQualityForExpiredItem (Item item) {
+        if (isExpired (item))
+            decrementQuality (item, DOUBLE_QUALITY_DECREMENT);
     }
 }
